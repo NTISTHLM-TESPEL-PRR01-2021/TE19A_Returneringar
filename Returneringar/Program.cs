ab49c1f9c338c9ddfdbdd04b305f75a1a3984d45
Mikael <mikael.bergstrom@ga.ntig.se>
2020-09-21 09:02:42 +0200

    Lade till metod

--- Returneringar/Program.cs
+++ Returneringar/Program.cs
@@ -2,11 +2,42 @@
 
 namespace Returneringar
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      // int i = 20;
+      // while (i > 0)
+      // {
+      //   Hello();
+      //   i--;
+      // }
+      int points = 0;
+
+      int test = Hello();
+      
+      points += test;
+
+      Console.WriteLine(points);
+
+      Console.ReadLine();
     }
+
+    static int Hello()
+    {
+      // string s = "hej";
+      // Console.WriteLine("HELLO!!!");
+
+      string g = Console.ReadLine();
+      if (g == "r")
+      {
+        return 1;
+      }
+      else
+      {
+        return 2;
+      }
+    }
+
+  }
 }
